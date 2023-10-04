14680a3c60a2d6d85c9d62b0b1fd32f3c1357b2b
karolb <beetinthestreet@gmail.com>
2023-10-04 19:27:53 +0200

    fixed html tags

--- public/views/Projects.php
+++ public/views/Projects.php
@@ -39,7 +39,7 @@
                 <div class="grid-element" id="grid-element1">
                     <img src="public/img/uploads/lorem.png" alt="books">
                     <div class="content-description">
-                        <h2>Lorem ipsum dolor sit amet</h2> 
+                        <h2>Lorem ipsum dolor sitttt amet</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo nisi sed auctor hendrerit. Aliquam in semper libero.</p>
                         <div class="social-section">
                             <i class="fas-fa heart"> 600 </i>
