2f8f38a83fea62970e40512d9d651ae27f8a75a3
KarolBochenek <95887356+KarolBochenek@users.noreply.github.com>
2023-10-14 14:02:39 +0200

    singleton

--- Database.php
+++ Database.php
@@ -6,6 +6,7 @@ class Database
     private $password;
     private $host;
     private $database;
+    private static $instance = null;
 
     public function __construct()
     {
@@ -32,4 +33,27 @@ class Database
             die("Connection failed: " . $e->getMessage());
         }
     }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+
+
 }
